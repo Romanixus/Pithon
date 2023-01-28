4a5007e758c6dde00988e3105fd2a29f2a8be4e5
Ogara <ya.rom4345@yandex.ru>
2023-01-28 16:50:42 +0200

    Обвязка

--- TeleBot/main.py
+++ TeleBot/main.py
@@ -9,5 +9,5 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 app = ApplicationBuilder().token("5593501461:AAEsF9GTIn60ZP-pZj6e9MW4AVkaY7WaOeQ").build()
 
 app.add_handler(CommandHandler("hello", hello))
-
+print('Bot is activated!')
 app.run_polling()
