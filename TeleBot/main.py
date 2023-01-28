165424ab8f95bc97671b9f693cc60134813234af
Ogara <ya.rom4345@yandex.ru>
2023-01-28 16:49:42 +0200

    Начинается

---
+++ TeleBot/main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5593501461:AAEsF9GTIn60ZP-pZj6e9MW4AVkaY7WaOeQ").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
